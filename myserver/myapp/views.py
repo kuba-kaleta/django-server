60d0cfdf847836f6d9176baf30901d7ad291e5b2
JakubKKK <kubamen9@wp.pl>
2022-06-05 19:42:16 +0200

    forms

--- myserver/myapp/views.py
+++ myserver/myapp/views.py
@@ -8,26 +8,29 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+                    item.save()
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
+                    print("invalid")
 
-                item.save()
-
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-
-    return render(response, "myapp/list.html", {"ls": ls})
+        return render(response, "myapp/list.html", {"ls": ls})
+    return render(response, "myapp/home.html", {})
 
 
 def home(response):
@@ -42,8 +45,13 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
 
         return HttpResponseRedirect("/%i" %t.id)
     else:
         form = CreateNewList()
     return render(response, "myapp/form.html", {"form": form})
+
+
+def view(response):
+    return render(response, "myapp/view.html", {})
